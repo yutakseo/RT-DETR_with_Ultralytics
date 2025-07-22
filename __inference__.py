c5cb91915bfbde630f4a840e57cb8c7549fb3bd5
Yutak Seo <yts@smartinside.ai>
2025-07-22 18:21:46 +0900

    25.07.22 First commit

---
+++ __inference__.py
@@ -0,0 +1,49 @@
+from .ultralytics.ultralytics import RTDETR
+import os, sys
+from datetime import datetime
+today = datetime.today().strftime("%y.%m.%d")
+
+model = RTDETR("/workspace/RT-DETR for construction equipment detection/25.07.22/weights/best.pt",)
+model.info()
+
+def image_inference(model, image_path):
+    results = model(
+        source=image_path,
+        project="Custom_RT-DETR",
+        name=f"/inference_results/{today}_result",
+        show=False,
+        save=True
+        )
+    return None
+
+def video_inference(model, video_path):
+    results = model(
+        source=video_path,
+        project="Custom_RT-DETR",
+        name=f"/inference_results/{today}_result",
+        stream=False,
+        save=True
+        )
+    return None
+
+
+#이미지 추론
+"""
+image_path = "/workspace/datasets"
+image_files = [os.path.join(image_path, fname) 
+               for fname in os.listdir(image_path) 
+               if fname.lower().endswith((".jpg", ".jpeg", ".png"))]
+
+for images in image_files:
+    image_inference(model, images)
+"""
+
+
+#영상 추론
+video_path = "/workspace/datasets"
+image_files = [os.path.join(video_path, fname) 
+               for fname in os.listdir(video_path) 
+               if fname.lower().endswith((".mp4", ".avi", ".mov"))]
+
+for videos in image_files:
+    image_inference(model, videos)
---
+++ __train__.py
@@ -0,0 +1,32 @@
+from .ultralytics.ultralytics import RTDETR
+from datetime import datetime
+today = datetime.today().strftime("%y.%m.%d")
+
+
+pt_model_path="/workspace/ultralytics/pre-trained models/rtdetr-x.pt"
+dataset_path="/workspace/datasets/ITONE_8Classes-constructionEquipments_dataset_refactoring/YOLO/data.yaml"
+
+def trainRTDETR(pt_model_path, dataset_path):
+    model = RTDETR(pt_model_path)
+    model.info()
+    results = model.train(
+        model=pt_model_path,
+        data=dataset_path, 
+        epochs=100,
+        batch=16,
+        imgsz=640,
+        save=True,
+        save_period=10,
+        device=[0,1],   
+        workers=8,
+        project="Custom_RT-DETR",
+        name=f"{today}_trained_model",
+        optimizer="auto",
+        val=True,
+        plots=True,
+        amp=True
+    )
+    return None
+
+if __name__ == "__main__":
+    trainRTDETR(pt_model_path, dataset_path)
